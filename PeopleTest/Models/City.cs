dd11a270c042ac8078b6ee49a71ef904637235d5
Åsa Jonsson <asaj71@gmail.com>
2022-12-05 21:54:54 +0100

    Add project files.

---
+++ PeopleTest/Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeopleTest.Models
+{
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }// Key
+
+
+        [Required]
+        [MaxLength(80)]
+        public string? CityName { get; set; }
+        public List<Person>? People { get; set; }//Navigation Property
+
+        public int CountryId { get; set; }// PeopleDbContext ForeignKey
+        public Country? Country { get; set; }//Navigation Property
+    }
+}
---
+++ PeopleTest/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeopleTest.Models
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }// Needs to be set in Cityclass 
+        public List<City>? Cities { get; set; }//Navigation Property
+        [Required]
+        [MaxLength(80)]
+        public string? CountryName { get; set; }
+    }
+}
